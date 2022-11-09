3b792e3e38fe589cc94b2ca00b2861ae87cd81a2
simple_29@hufs.ac.kr <simple_29hufs.ac.kr>
2022-11-09 12:13:04 +0900

    1109class

--- App.js
+++ App.js
@@ -1,59 +1,13 @@
 import React from "react";
-import {Button, View, ScrollView, StyleSheet } from "react-native";
+import { Button, View, ScrollView, StyleSheet, Text, Image, ImageBackground } from "react-native";
+import Constants from 'expo-constants';
+import { useState } from "react";
+import MainStackNavigator from "./routes/MainStackNavigator";
 
 const App = () => {
-  return (<>
-  <View style={[styles.container, styles.playingSpace]}>
+  
+  return <MainStackNavigator/>;
+ 
+};
 
-  </View>
-  <Scrollview style={[styles.container, styles.controlSpace]}>
-    <View style={[styles.buttonView]}>
-      <Button title="1"/>
-    </View>
-    <View style={[styles.buttonView]}>
-      <Button title="2"/>
-    </View>
-    <View style={[styles.buttonView]}>
-      <Button title="3"/>
-    </View>
-    <View style={[styles.buttonView]}>
-      <Button title="4"/>
-    </View>
-    <View style={[styles.buttonView]}>
-      <Button title="5"/>
-    </View>
-    <View style={[styles.buttonView]}>
-      <Button title="6"/>
-    </View>
-    <View style={[styles.buttonView]}>
-     <Button title="7"/>
-    </View>
-
-
-  </Scrollview>
-  </>);
-}
-
-const styles = StyleSheet.create({
-  container: {
-    height:'50%'
-  },
-  playingSpace: {
-    backgroundColor: 'white',
-    borderColor: 'blue',
-    borderwidth: 3
-
-  },
-  controlSpace:{
-    backgroundColor: '#5A5A5'
-    flexDirection: 'row'
-    flexWrap: 'wrap'
-  },
-  buttonView: {
-
-    width:'50%',
-    padding:10
-
-  }
-})
 export default App;
